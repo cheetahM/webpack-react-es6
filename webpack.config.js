dc5a1d19d9ec501a9fdb471deba65f06c1d69e20
chmeshra <chmeshra@visa.com>
2016-01-07 17:49:59 -0800

    first React component and adding dependencies in package json

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,14 @@ const common = {
   // Entry accepts a path or an object of entries.
   // The build chapter contains an example of the latter.
   entry: PATHS.app,
+
+  // Add resolve.extensions. '' is needed to allow imports
+ // without an extension. Note the .'s before extensions!!!
+ // The matching will fail without!
+ resolve: {
+   extensions: ['', '.js', '.jsx']
+ },
+
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
@@ -26,12 +34,24 @@ const common = {
       loaders: ['style', 'css'],
       // Include accepts either a path or an array of paths.
       include: PATHS.app
-    }
+    },
+    // Set up jsx. This accepts js too thanks to RegExp
+     {
+       test: /\.jsx?$/,
+       // Enable caching for improved performance during development
+       // It uses default OS directory by default. If you need something
+       // more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
+       loaders: ['babel?cacheDirectory'],
+       include: PATHS.app
+     }
   ]
 },
   plugins: [
     new HtmlwebpackPlugin({
-      title: 'React-webpact-ES6 starter kit app'
+      template: 'node_modules/html-webpack-template/index.html',
+      title: 'React-webpact-ES6 starter kit app',
+      appMountId: 'app'
+
     })
   ]
 };
